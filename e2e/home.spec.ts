53760ec4060dab2d44aa48d65dff2ec2f73d78cd
ImpstudioWav <larsmikkelhansen18@gmail.com>
2026-04-11 12:44:47 +0100

    chore: add CI, Vitest, and Playwright setup

---
+++ e2e/home.spec.ts
@@ -0,0 +1,8 @@
+import { expect, test } from "@playwright/test";
+
+test("homepage loads", async ({ page }) => {
+  await page.goto("/");
+  await expect(
+    page.getByRole("heading", { name: /get started/i }),
+  ).toBeVisible();
+});
