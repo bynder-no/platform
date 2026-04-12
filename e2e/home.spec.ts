996d664ef1527175aaf1e9da8f07b5f4d5cd26da
ImpstudioWav <larsmikkelhansen18@gmail.com>
2026-04-12 10:38:44 +0100

    Fix homepage e2e test

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -2,7 +2,6 @@ import { expect, test } from "@playwright/test";
 
 test("homepage loads", async ({ page }) => {
   await page.goto("/");
-  await expect(
-    page.getByRole("heading", { name: /get started/i }),
-  ).toBeVisible();
+  await expect(page.getByRole("heading", { name: "Listings" })).toBeVisible();
+  await expect(page.getByRole("link", { name: "Login" })).toBeVisible();
 });
